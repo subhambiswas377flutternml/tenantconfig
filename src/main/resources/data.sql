3d4b9adcec9cc5de6ebe5e1d0c81cd71953de56f
subhambiswas <subham.biswas@innofied.com>
2025-12-02 11:50:59 +0530

    some changes

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,5 @@
 insert into Tenants(ANDROID_API_KEY,ANDROID_APP_ID,ANDROID_PROJECT_ID,ANDROID_SENDER_ID,ANDROID_STORAGE_BUCKET,IOS_API_KEY,IOS_APP_ID,IOS_PROJECT_ID,IOS_SENDER_ID,IOS_STORAGE_BUCKET,LOGIN_LOGO,ON_PRIMARY,PRIMARY_COLOR,TENANT_ID,BUNDLE_ID)
-values('AIzaSyB-wkxYczoN2PfzM5hKxTetKqZBCGnzo4o','1:87096992381:android:91264666f9cd3d27f60714','allride-production','87096992381','allride-production.firebasestorage.app','AIzaSyD2M1OsQ6o6lLvUPdEK7dcfeio55CUn-fg','1:87096992381:ios:2036ac1a29104823f60714','allride-production','87096992381','allride-production.firebasestorage.app','https://i.ibb.co/qhGmBX7/login-logo.png','#FFFFFF','#e5bd6c','be4192ae-dd60-4a02-8f1f-5530b012a91e','com.app.uniroutesshuttle');
+values('AIzaSyB-wkxYczoN2PfzM5hKxTetKqZBCGnzo4o','1:87096992381:android:91264666f9cd3d27f60714','allride-production','87096992381','allride-production.firebasestorage.app','AIzaSyD2M1OsQ6o6lLvUPdEK7dcfeio55CUn-fg','1:87096992381:ios:2036ac1a29104823f60714','allride-production','87096992381','allride-production.firebasestorage.app','https://i.ibb.co/qhGmBX7/login-logo.png','#FFFFFF','#e5bd6c','be4192ae-dd60-4a02-8f1f-5530b012a91e','com.app.uniroutes');
 
 insert into Tenants(
     ANDROID_API_KEY,
